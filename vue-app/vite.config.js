be288b5eb4d0920d02cb299169433ab54651de4e
usedchang <@example.com>
2026-04-16 22:00:15 +0800

    fix: 修复highlight.js vite解析报错

--- vue-app/vite.config.js
+++ vue-app/vite.config.js
@@ -1,7 +1,22 @@
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+// })
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // 👇 新增这两行，强制解决 Rolldown 解析报错
+  optimizeDeps: {
+    include: ['highlight.js']
+  },
+  build: {
+    rollupOptions: {
+      external: ['highlight.js']
+    }
+  }
 })
